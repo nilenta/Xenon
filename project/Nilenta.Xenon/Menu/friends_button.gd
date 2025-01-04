87b38c08221819289d38966cc1e18887c5963784
Legacy <44476311+FerretPaws@users.noreply.github.com>
2025-01-03 20:42:48 -0500

    Delete project/Nilenta.Xenon directory

--- project/Nilenta.Xenon/Menu/friends_button.gd
+++
@@ -1,7 +0,0 @@
-class_name FriendsButton
-extends GenericUIButton
-
-
-func _on_friends_pressed() -> void:
-	get_parent().get_parent().get_node("friends_menu").visible = true
-	return
--- project/Nilenta.Xenon/Menu/xenon_button.gd
+++
@@ -1,7 +0,0 @@
-class_name XenonButton
-extends GenericUIButton
-
-
-func _on_xenon_pressed() -> void:
-	get_parent().get_parent().get_node("xenon_menu").visible = true
-	return
